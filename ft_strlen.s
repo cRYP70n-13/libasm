c2eddfd87715430df71915a22791bb85ae9bc281
cRYP70n-13 <otmane.kimdil@gmail.com>
2021-04-28 19:03:03 +0000

    Adding Strlen and strcpy

--- ft_strlen.s
+++ ft_strlen.s
@@ -6,4 +6,10 @@ _ft_strlen:
     xor rax, rax ; Initialize the rax to 0
 
 _while:
-    cmp BYTE[rax + rdi]
+    cmp BYTE[rax + rdi], 0
+	je	_return				; if *(str + rax) == 0 jump to _return
+	inc rax					; Else increment the counter
+	jmp _while				; repeat _loop
+
+_return:
+	ret						; return the len of the string
--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include "libasm.h"
+#include <string.h>
 
 int main(void) {
     int fd = open("./libasm.h", O_RDONLY);
     char *buff = malloc(11);
     int ret = ft_read(-1, buff, 4);
+	int len = ft_strlen("Hey this is me Otmane Kimdil");
+	int correctLen = strlen("Hey this is me Otmane Kimdil");
 
-    printf("%s\n", strerror(errno));
-    printf("%s\n", buff);
-    printf("%d\n", ret);
+	printf("Test ERRNO %s\n", strerror(errno));
+    printf("Test READ %s\n", buff);
+    printf("Test WRITE %d\n", ret);
+    printf("Test STRLEN %d\n", len);
+    printf("Test STRLEN %d\n", correctLen);
 
     ret = ft_write(1, "Hey Elfassi\n", 13);
     printf("%d\n", ret);
