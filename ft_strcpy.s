594ef61a1a452fe4b815af015767ccd0464c76c3
cRYP70n-13 <otmane.kimdil@gmail.com>
2021-05-03 14:42:29 +0000

    Adding strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -2,17 +2,17 @@ section .text
 	global _ft_strcpy
 
 _ft_strcpy:
-	xor rcx, rcx ; Just a counter set to 0
+	xor rcx, rcx 					; Just a counter set to 0
 
 _while:
-	cmp		BYTE [rsi + rcx], 0		; check *(s + rcx) Is NULL '\0'
+	cmp		BYTE[rsi + rcx], 0		; check *(s + rcx) Is NULL '\0'
 	je		_return					; if yes, jump _return
-	mov		dl, BYTE [rsi + rcx]	; else, copy character in dst into dl
-	mov		BYTE [rdi + rcx], dl	; copy character in dl into s
+	mov		dl, BYTE[rsi + rcx]		; else, copy character in dst into dl
+	mov		BYTE[rdi + rcx], dl		; copy character in dl into s
 	inc		rcx						; counter++
 	jmp		_while					; repeat loop
 
 _return:
-	mov		BYTE [rdi, rcx], 0		; put '\0' at end of dst
+	mov		BYTE[rdi + rcx], 0		; put '\0' at end of dst
 	mov		rax, rdi				; put dst in rax
 	ret								; return of function (rax)
--- main.c
+++ main.c
@@ -2,20 +2,24 @@
 #include <string.h>
 
 int main(void) {
-    int fd = open("./libasm.h", O_RDONLY);
-    char *buff = malloc(11);
-    int ret = ft_read(-1, buff, 4);
-	int len = ft_strlen("Hey this is me Otmane Kimdil");
-	int correctLen = strlen("Hey this is me Otmane Kimdil");
+	// int fd = open("./libasm.h", O_RDONLY);
+	// char *buff = malloc(11);
+	// int ret = ft_read(-1, buff, 4);
+	// int len = ft_strlen("Hey this is me Otmane Kimdil");
+	// int correctLen = strlen("Hey this is me Otmane Kimdil");
+	const char *src = "Hello Otmane Kimdil";
+	char *dst = malloc(20);
 
-	printf("Test ERRNO %s\n", strerror(errno));
-    printf("Test READ %s\n", buff);
-    printf("Test WRITE %d\n", ret);
-    printf("Test STRLEN %d\n", len);
-    printf("Test STRLEN %d\n", correctLen);
+	char *res = ft_strcpy(dst, src);
+	printf("Test STRCPY %s\n", res);
+	// printf("Test ERRNO %s\n", strerror(errno));
+	// printf("Test READ %s\n", buff);
+	// printf("Test WRITE %d\n", ret);
+	// printf("Test STRLEN %d\n", len);
+	// printf("Test STRLEN %d\n", correctLen);
 
-    ret = ft_write(1, "Hey Elfassi\n", 13);
-    printf("%d\n", ret);
+	// ret = ft_write(1, "Hey Elfassi\n", 13);
+	// printf("%d\n", ret);
 
-    return 0;
+	return 0;
 }
